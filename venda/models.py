1bc23ffdf07af1459fa202502bd1c3f4815f7028
pedro <pedroh.mix@gmail.com>
2020-09-03 15:59:53 -0300

    Definindo modelos

--- venda/models.py
+++ venda/models.py
@@ -6,10 +6,10 @@ from datetime import datetime
 
 
 class Venda(models.Model):
-    produto = models.ForeignKey(Produto,on_delete=models.CASCADE)
+    produto = models.ForeignKey(Produto, on_delete=models.CASCADE)
     valor = models.DecimalField(max_digits=6, decimal_places=2)
     data_efetivada = models.DateTimeField(default=datetime.now, blank=True)
-    indicacao = models.ForeignKey(Indicacao,on_delete=models.CASCADE)
+    indicacao = models.ForeignKey(Indicacao, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.produto
