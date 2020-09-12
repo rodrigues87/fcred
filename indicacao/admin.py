3c3a3d8450ab2c7517f466f66257fd2a97bcb7ee
pedro <pedroh.mix@gmail.com>
2020-09-12 14:43:48 -0300

    Listando indicações

--- indicacao/admin.py
+++ indicacao/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 
 from indicacao.models import Indicacao
-list_display = ('prospector', 'nome_indicado', 'telefone_indicado','efetivada','data_indicacao','status')
-admin.site.register(Indicacao)
+
+
+class IndicacaoAdmin(admin.ModelAdmin):
+    list_display = ('prospector', 'nome_indicado', 'telefone_indicado', 'efetivada', 'data_indicacao', 'status')
+
+
+admin.site.register(Indicacao,IndicacaoAdmin)
